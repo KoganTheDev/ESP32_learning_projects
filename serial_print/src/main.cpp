eed4dc5bfc03d2037977abb55723fb3e7851206a
KoganTheDev <Yuvalkogan123@gmail.com>
2025-12-16 21:26:59 +0200

    Add Doorbell (Active buzzer), Add Serial Monitor

---
+++ serial_print/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ESP32 initialization completed!");
+}
+
+void loop() {
+  Serial.printf("Running time : %.1f s\n", millis() / 1000.0f);
+  delay(1000);
+}
